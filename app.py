b42ee6f7984cb70e34bb3e454d8bc8fb6f0513ef
abu-sayeed-bhuiyan <abbfeni@gmail.com>
2026-09-16 19:54:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ STATIONS_META = [
     (17, "Khowai", "Habiganj", "HABIGANJ", 9.00),
 ]
 
-raw_data = st.text_area("Insert filed data ( SMS format must be :  Station *Date *6PM(WL) *6AM(WL) *9AM(WL) *Rainfall( if any))", height=180, placeholder="Kan*150926*1050*1021*1016*0.0\n...")
+raw_data = st.text_area("Insert field data ( SMS format must be :  Station *Date *6PM(WL) *6AM(WL) *9AM(WL) *Rainfall( if any))", height=180, placeholder="Kan*150926*1050*1021*1016*0.0\n...")
 
 if st.button("Generate Bulletin & PDF"):
     if not raw_data.strip():
